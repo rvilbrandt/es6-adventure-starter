e73214e6fc398c3a1e799c5362b146c4d2ef0906
rvilbrandt <info@rvi-media.de>
2020-11-04 16:06:04 +0100

    2 files modified, 2 added

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 // Import stylesheets
 import "./style.css";
+import rooms from "./rooms";
+import verbindungen from "./verbindungen";
 
 // Write Javascript code!
-const app = document.getElementById("app");
+var app = document.getElementById("app");
+var img = document.getElementById("room");
+var desc = document.getElementById("desc");
+var nav = document.getElementById("nav");
+
+function showRoom(room) {
+  img.src = room.image;
+  desc.innerHTML = room.description;
+  nav.innerHTML = "";
+
+  var button = document.createElement("button");
+  button.innerText = "N";
+
+  nav.append(button);
+}
+
+showRoom({});
---
+++ rooms.js
@@ -0,0 +1,82 @@
+export default [
+  {
+    id: "01",
+    name: "Ärztezimmer",
+    type: 1,
+    image: "https://loremflickr.com/800/600/doctor,room?random=",
+    description:
+      "Ein scharfer Schmerz durchzuckt deinen Schädel, als du die Augen öffnest. Du kannst grade so erkennen, dass du dich in einer Art Büro befindest, bevor sich dein Kopf erneut meldet und deine Hand an die Stirn zuckt. Und damit direkt in eine Wunde."
+  },
+  {
+    id: "02",
+    name: "Toilette",
+    type: 0,
+    image: "https://loremflickr.com/800/600/toilet,hospital?random=",
+    description:
+      "Als du den Raum betrittst, kommt dir der Geruch von abgestandenem Wasser und Urin entgegen. Angeekelt stellst du fest, dass du über (hoffentlich) tierische Exkremente steigen musst, um in den nächsten Raum zu gelangen."
+  },
+  {
+    id: "03",
+    name: "Flur",
+    type: 0,
+    image: "https://loremflickr.com/800/600/corridor,hospital?random=",
+    description:
+      'Durch die riesigen Fenster fällt das grelle Sonnenlicht und du glaubst, den Schriftzug "Intensiv Station" an der Wand ausmachen zu können. Mit einem lauten Knacken zerbrechen Scherben unter deinen Füßen, als du dich zur nächsten Tür bewegst.'
+  },
+  {
+    id: "04",
+    name: "Behandlungsraum",
+    type: 0,
+    image: "https://loremflickr.com/800/600/operation+room,hospital?random=",
+    description:
+      "Vieräugig starrt dich die alte Lampe an, als du den Raum betrittst. Mit jedem Schritt scheinst du neue Fläschen umzustoßen, die klirrend über den Boden rollen, um dann zwischen alten Zeitschriften zum Stehen zu kommen."
+  },
+  {
+    id: "05",
+    name: "Patientenraum",
+    type: 0,
+    image: "https://loremflickr.com/800/600/room,hospital?random=",
+    description:
+      "Du versuchst deinen Blick von den Flecken zu reißen, die sich wie ein Muster über die vergilbten Laken räkeln. Die Betten, die unter den geöffneten Fenstern stehen, danken dies mit Schimmelkolonien und dir wird klar, dass du hier wohl kein Nachtlager aufschlagen kannst."
+  },
+  {
+    id: "06",
+    name: "Treppenhaus",
+    type: 0,
+    image: "https://loremflickr.com/800/600/stairs,hospital?random=",
+    description:
+      "Die grellen Graffitis erinnern dich daran, dass du nicht der einzige Mensch bist, der diesen Ort je betreten hat. Doch das bisschen Erleichterung verschwindet sofort, als du die Tür hinter dir ins Schloss fallen hörst."
+  },
+  {
+    id: "07",
+    name: "Keller",
+    type: 0,
+    image: "https://loremflickr.com/800/600/cellar,hospital?random=",
+    description:
+      "Die Kälte kriecht aus den Mauern in deine Knochen und die Luft wird mit jedem Schritt dicker. Du hoffst, dass das Licht reicht, um einen Ausgang zu finden, denn du bist dir sicher, dass du hier nicht bleiben willst."
+  },
+  {
+    id: "08",
+    name: "Kapelle",
+    type: 0,
+    image: "https://loremflickr.com/800/600/chapel,hospital?random=",
+    description:
+      "Weder Kreuze noch Bibeln findest du in der verlassenen Kapelle. Dafür jedoch ein fette Ratte, die sich in der Ecke einquatiert hat und dort seelenruhig an etwas herumkaut, dessen Ursprung du lieber nicht wissen willst..."
+  },
+  {
+    id: "09",
+    name: "Leichenhalle",
+    type: 0,
+    image: "https://loremflickr.com/800/600/mortuary,hospital?random=",
+    description:
+      "Du brauchst einen Moment, um die dunklen Flecken hinter dir zu identifizieren, doch sobald es dir klar wird, schießt dir die Galle in den Mund. Still betend, dass deiner der einzige menschliche Körper in der Leichenhalle ist, stolperst du auf die nächste Tür zu."
+  },
+  {
+    id: "10",
+    name: "Garten",
+    type: 2,
+    image: "https://loremflickr.com/800/600/garden,hospital?random=",
+    description:
+      "Der Anblick der Picknicktische ruft dir das ins Gedächtnis, was dein Adrenalin bisher überschattet hat und wie auf Befehl rumort dein Magen. Doch weder das, noch das Stechen in deiner Lunge lassen dich langsamer werden, bis du das Krankenhaus hinter dich gebracht hast."
+  }
+];
